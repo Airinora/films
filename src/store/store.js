cacfe5db9c9501d6d76d91274979c59252caa4c8
oantropova <oantropova@htc-cs.ru>
2019-04-12 12:49:47 +0400

    Local Storage, добавление и удаление фильмов

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import reducers from '../reducers';
+import {addFilmToLocalStorage, getFromLocalStorage, removeFilmFromLocalStorage} from "./local-storage";
 
 // Redux dev tools.
 // See more: https://github.com/zalmoxisus/redux-devtools-extension
@@ -11,8 +12,17 @@ function getDevTools() {
         : f => f;
 }
 
-export function configureStore(initialState = {}) {
-    let middlewares = [thunk];
+const stateFromLocalStorage = {
+    watchedFilms: {
+        watched: getFromLocalStorage('watched')
+    },
+    favouriteFilms: {
+        favourite: getFromLocalStorage('favourite')
+    }
+};
+
+export function configureStore(initialState = stateFromLocalStorage) {
+    let middlewares = [thunk, addFilmToLocalStorage, removeFilmFromLocalStorage];
 
     let enhanser = compose(
         applyMiddleware(...middlewares),
